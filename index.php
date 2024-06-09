590124294e8ae8d27a30a6d0eb1536cc0c278b67
Zen1ne <zenaku1ne@gmail.com>
2024-06-08 18:19:49 -0600

    se crean nuevos arvhicos

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+error_reporting(E_ALL);
+ini_set('ignore_repeated_errors',TRUE);
+ini_set('display_errors',FALSE);
+ini_set('log_errors',TRUE);
+ini_set("error_log","/var/www/html/laravel-proyect/login-mvc/ERRORS/php-error.log");
+error_log("Inicio de aplicacion WEB");
+
+
+
+require_once 'libs/app.php';
+
+
+?> 
